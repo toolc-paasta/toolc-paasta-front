4f70210aad354ed4c51159f5fac9e31bace7454b
shkim787 <shkim061198@gmail.com>
2021-09-07 22:52:58 +0900

    feat : insert Contact

--- src/components/elements/data.js
+++ src/components/elements/data.js
@@ -1,48 +1,57 @@
 export const temp_data = {
-    kg_name:'좋은 유치원',
-    user_name:'봉미선',
-    type:'pr',                      //pr,tc,ad
-    auth:true,
-  }
-  
+  kg_name:'좋은 유치원',
+  user_name:'봉미선',
+  type:'pr',                      //pr,tc,ad
+  auth:true,
+}
+
+export const users = [
+{id: 0, name: '김성현', contact:'01043376752'},
+{id: 0, name: '가나다', contact:'01012345678'},
+{id: 0, name: '라마바', contact:'01098765432'},
+{id: 0, name: '사아자', contact:'01013579246'},
+{id: 0, name: '치키챠', contact:'010345782345'},
+{id: 0, name: '아이린', contact:'01010293847'},
+]
+
 export const btns_for_pr = [
-  {id: 0, title: '가정통신문 확인', img:'book-outline'},
-  {id: 1, title: '학부모 게시판', img:'clipboard-outline'},
-  {id: 2, title: '넌 광야를', img:'logo-twitch'},
-  {id: 3, title: '떠돌고 있어', img:'logo-youtube'},
-  {id: 4, title: '나의 분신을', img:'logo-github'},
-  {id: 5, title: '찾고 싶어', img:'logo-react'},
+{id: 0, title: '가정통신문 확인', img:'book-outline'},
+{id: 1, title: '학부모 게시판', img:'clipboard-outline'},
+{id: 2, title: '넌 광야를', img:'logo-twitch'},
+{id: 3, title: '떠돌고 있어', img:'logo-youtube'},
+{id: 4, title: '나의 분신을', img:'logo-github'},
+{id: 5, title: '찾고 싶어', img:'logo-react'},
 ];
 
 export const btns_for_ad = [
-  {id: 0, title: '가정통신문 보내기', img:'book-outline'},
-  {id: 1, title: '개인알림 보내기', img:'mail-outline'},
-  {id: 2, title: '전화번호부 보기', img:'call-outline'},
-  {id: 3, title: '차량 관리', img:'car-outline'},
-  {id: 4, title: '학부모 게시판', img:'clipboard-outline'},
-  {id: 5, title: '인증 관리', img:'log-in-outline'},
+{id: 0, title: '가정통신문 보내기', img:'book-outline'},
+{id: 1, title: '개인알림 보내기', img:'mail-outline'},
+{id: 2, title: '전화번호부 보기', img:'call-outline'},
+{id: 3, title: '차량 관리', img:'car-outline'},
+{id: 4, title: '학부모 게시판', img:'clipboard-outline'},
+{id: 5, title: '인증 관리', img:'log-in-outline'},
 ];
 
 export const current_list = [
-  {time: new Date(2021, 8, 1, 14, 49, 5), title: '학부모 게시판', content:'wow'},
-  {time: new Date(2021, 8, 1, 14, 4, 5), title: '가정통신문 확인', content:'네 알려드렸습니다~~'},
-  {time: new Date(2021, 8, 1, 3, 4, 5), title: '학부모 게시판', content:'응애'},
-  {time: new Date(2021, 7, 28, 3, 4, 5), title: '가정통신문 확인', content:'0829 알려드립니다'},
-  {time: new Date(2021, 7, 27, 3, 4, 5), title: '비트코인 갤러리', content:'이더리움 클래식 수익 500% 인증'},
+{time: new Date(2021, 8, 1, 14, 49, 5), title: '학부모 게시판', content:'wow'},
+{time: new Date(2021, 8, 1, 14, 4, 5), title: '가정통신문 확인', content:'네 알려드렸습니다~~'},
+{time: new Date(2021, 8, 1, 3, 4, 5), title: '학부모 게시판', content:'응애'},
+{time: new Date(2021, 7, 28, 3, 4, 5), title: '가정통신문 확인', content:'0829 알려드립니다'},
+{time: new Date(2021, 7, 27, 3, 4, 5), title: '비트코인 갤러리', content:'이더리움 클래식 수익 500% 인증'},
 ];
 
 export const list1 = [
-  {time: new Date(2021, 8, 1, 14, 49, 5), title: 'wow', content:'Im addicted, 끊임없이 \n말을 걸어주는 나의 에스파 \n이런 교감, 너의 존잰 날 \n다른 차원으로 이끌었지 (oh) \n\n난 세상 중심에 \nYou re in the flat \n내 모든 action \n어린 너의 힘을 키워'},
-  {time: new Date(2021, 8, 1, 14, 4, 5), title: '네 알려드렸습니다~~', content:'넌 언젠가부터 Synk out \n노이즈같이 보여 \n더 이상 못 찾겠어 \n널 유혹해 삼킨 건, black mamba'},
-  {time: new Date(2021, 8, 1, 3, 4, 5), title: '응애', content:'Im on the next level yeah \n절대적 룰을 지켜 \n내 손을 놓지 말아 \n결속은 나의 무기 \n광야로 걸어가 \n알아 네 home ground \n위협에 맞서서 \n제껴라 제껴라 제껴라'},
-  {time: new Date(2021, 7, 28, 3, 4, 5), title: '0829 알려드립니다', content:'상상도 못한 black out \n유혹은 깊고 진해 \n(Too hot too hot) (Ooh ooh wee) \n맞잡은 손을 놓쳐 \n난 절대 포기 못해'},
-  {time: new Date(2021, 7, 27, 3, 4, 5), title: '룰루야 드가자!!', content:'Beat drop Naevis, calling \n절대로 뒤를 돌아보지 말아 \n광야의 것 탐내지 말아 \n약속이 깨지면 모두 걷잡을 수 없게 돼 \n언제부턴가 불안해져 가는 신호 \n널 파괴하고 말 거야 (we want it) \nCome on! Show me the way \nto Kosmo yeah yeah'}
+{time: new Date(2021, 8, 1, 14, 49, 5), title: 'wow', content:'Im addicted, 끊임없이 \n말을 걸어주는 나의 에스파 \n이런 교감, 너의 존잰 날 \n다른 차원으로 이끌었지 (oh) \n\n난 세상 중심에 \nYou re in the flat \n내 모든 action \n어린 너의 힘을 키워'},
+{time: new Date(2021, 8, 1, 14, 4, 5), title: '네 알려드렸습니다~~', content:'넌 언젠가부터 Synk out \n노이즈같이 보여 \n더 이상 못 찾겠어 \n널 유혹해 삼킨 건, black mamba'},
+{time: new Date(2021, 8, 1, 3, 4, 5), title: '응애', content:'Im on the next level yeah \n절대적 룰을 지켜 \n내 손을 놓지 말아 \n결속은 나의 무기 \n광야로 걸어가 \n알아 네 home ground \n위협에 맞서서 \n제껴라 제껴라 제껴라'},
+{time: new Date(2021, 7, 28, 3, 4, 5), title: '0829 알려드립니다', content:'상상도 못한 black out \n유혹은 깊고 진해 \n(Too hot too hot) (Ooh ooh wee) \n맞잡은 손을 놓쳐 \n난 절대 포기 못해'},
+{time: new Date(2021, 7, 27, 3, 4, 5), title: '룰루야 드가자!!', content:'Beat drop Naevis, calling \n절대로 뒤를 돌아보지 말아 \n광야의 것 탐내지 말아 \n약속이 깨지면 모두 걷잡을 수 없게 돼 \n언제부턴가 불안해져 가는 신호 \n널 파괴하고 말 거야 (we want it) \nCome on! Show me the way \nto Kosmo yeah yeah'}
 ];
 
 export const comment =[
-  {time: new Date(2021, 8, 4, 14, 49, 5), nickname:'서연맘', content: 'ㅋㅋㅇㅈ',},
-  {time: new Date(2021, 8, 3, 14, 49, 5), nickname:'연서맘', content: 'ㄹㅇㅋㅋ2',},
-  {time: new Date(2021, 8, 2, 14, 49, 5), nickname:'주현맘', content: 'ㅋㅋㅇㅈ3',},
-  {time: new Date(2021, 8, 1, 14, 49, 5), nickname:'민준맘', content: 'ㅋㅋㅇㅈ4',},
-  {time: new Date(2021, 8, 1, 11, 49, 5), nickname:'경훈맘', content: 'ㅋㅋㅇㅈ5',},
+{time: new Date(2021, 8, 4, 14, 49, 5), nickname:'서연맘', content: 'ㅋㅋㅇㅈ',},
+{time: new Date(2021, 8, 3, 14, 49, 5), nickname:'연서맘', content: 'ㄹㅇㅋㅋ2',},
+{time: new Date(2021, 8, 2, 14, 49, 5), nickname:'주현맘', content: 'ㅋㅋㅇㅈ3',},
+{time: new Date(2021, 8, 1, 14, 49, 5), nickname:'민준맘', content: 'ㅋㅋㅇㅈ4',},
+{time: new Date(2021, 8, 1, 11, 49, 5), nickname:'경훈맘', content: 'ㅋㅋㅇㅈ5',},
 ]
