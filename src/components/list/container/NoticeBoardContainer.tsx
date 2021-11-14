18bf25b92b4d1c904f60dac6b1885ce2112a0303
shkim787 <shkim061198@gmail.com>
2021-11-14 16:39:59 +0900

    tmp

--- src/components/list/container/NoticeBoardContainer.tsx
+++ src/components/list/container/NoticeBoardContainer.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BottomTabNavigation } from "../../../screens/NoticeBoardScreen";
 import { RootState } from "../../../modules";
 import { useDispatch, useSelector } from "react-redux";
 import List from "../view/List";
+import { getNotice } from "../../../lib/api/forAdmin"
 
 type Props = {
    navigation: BottomTabNavigation;
@@ -12,11 +12,22 @@ type Props = {
 function noticeBoardContainer({ navigation }: Props) {   
 
    const auth =  useSelector(({auth} :RootState) => auth)
-   useEffect(() => {
+   const [list, setList] = useState<any>();
+
+   const getData = async() => {
+      const data = await getNotice();
+      data.map((item:any, i:number) => (
+         console.log(item)
+      ))
       
+      setList(data)
+   }
+
+   useEffect(() => {
+      getData()
    }, []);
 
-   return <List navigation={navigation} headerTitle={'공지 모아보기'} auth={auth}/>;
+   return <List navigation={navigation} headerTitle={'공지 모아보기'} auth={auth} list={list}/>;
 }
 
 export default noticeBoardContainer;
