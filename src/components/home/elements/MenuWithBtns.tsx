801cfd1078ef4cfa3ea531df25d154455fbd7a7a
Seongil-Shin <callmeshin75@gmail.com>
2021-11-14 16:34:24 +0900

    feat : delete talk on director

--- src/components/home/elements/MenuWithBtns.tsx
+++ src/components/home/elements/MenuWithBtns.tsx
@@ -1,85 +1,95 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet,TouchableOpacity,Dimensions } from 'react-native';
-import { btns_for_pr } from '../../elements/data';
-import { btns_for_ad } from '../../elements/data';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {
+   Text,
+   View,
+   StyleSheet,
+   TouchableOpacity,
+   Dimensions,
+} from "react-native";
+import { btns_for_di, btns_for_pr } from "../../elements/data";
+import { btns_for_ad } from "../../elements/data";
+import Icon from "react-native-vector-icons/Ionicons";
 import { BottomTabNavigation } from "../../../screens/SearchScreen";
 
 type Btn = {
-  id:number;
-  title:string;
-  img:string;
-  onclick:string;
-}
-
-type Props = {
-  user_type:string;
-  navigation:BottomTabNavigation;
-
+   id: number;
+   title: string;
+   img: string;
+   onclick: string;
 };
 
-const btnPr:Btn[] = btns_for_pr;
-const btnAd:Btn[] = btns_for_ad;
+type Props = {
+   user_type: string;
+   navigation: BottomTabNavigation;
+};
 
-export default function MenuWithBtns({user_type,navigation}:Props) {
+const btnPr: Btn[] = btns_for_pr;
+const btnAd: Btn[] = btns_for_ad;
+const btnDi: Btn[] = btns_for_di;
 
-  const [authType,setAuthType] = useState<any>()
-  
-  useEffect(() => {
-    user_type==='PARENT' ? setAuthType(btnPr) : setAuthType(btnAd)
-  }, []);
+export default function MenuWithBtns({ user_type, navigation }: Props) {
+   const [authType, setAuthType] = useState<any>();
 
-  return (
-    <>
-      <View style={styles.row}>
-        {authType?.map((item :any, i :any) => (
-          <View style={styles.btnContainer} key={i}>
-            <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate(item.onclick)}>
-                <Icon
-                  name={item.img}
-                  size={35}
-                  color="black"
-                  style={styles.icon}
-                />
-            </TouchableOpacity>
-            <View style={styles.textView}>
-              <Text style={styles.text}>{item.title}</Text>
-            </View>
-          </View>  
-        ))}
-      </View>
-    </>
-  );
+   useEffect(() => {
+      user_type !== "PARENT"
+         ? user_type === "DIRECTOR"
+            ? setAuthType(btnDi)
+            : setAuthType(btnAd)
+         : setAuthType(btnPr);
+   }, []);
+
+   return (
+      <>
+         <View style={styles.row}>
+            {authType?.map((item: any, i: any) => (
+               <View style={styles.btnContainer} key={i}>
+                  <TouchableOpacity
+                     style={styles.btn}
+                     onPress={() => navigation.navigate(item.onclick)}>
+                     <Icon
+                        name={item.img}
+                        size={35}
+                        color="black"
+                        style={styles.icon}
+                     />
+                  </TouchableOpacity>
+                  <View style={styles.textView}>
+                     <Text style={styles.text}>{item.title}</Text>
+                  </View>
+               </View>
+            ))}
+         </View>
+      </>
+   );
 }
 
 const styles = StyleSheet.create({
-  row:{
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    paddingVertical: 8,
-  },
-  btnContainer:{
-    width:Dimensions.get('window').width * 0.8 / 3 - 10,
-    height: 100,
-    alignItems: 'center',
-  },
-  btn:{
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    backgroundColor:'#FEE9B0',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 8
-  },
-  icon:{
-    fontSize:25
-  },
-  textView:{
-  },
-  text:{
-      textAlign:'center',
-      fontSize: 10
-  },
+   row: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      paddingVertical: 8,
+   },
+   btnContainer: {
+      width: (Dimensions.get("window").width * 0.8) / 3 - 10,
+      height: 100,
+      alignItems: "center",
+   },
+   btn: {
+      width: 60,
+      height: 60,
+      borderRadius: 100,
+      backgroundColor: "#FEE9B0",
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 8,
+   },
+   icon: {
+      fontSize: 25,
+   },
+   textView: {},
+   text: {
+      textAlign: "center",
+      fontSize: 10,
+   },
 });
--- src/components/home/view/Home.tsx
+++ src/components/home/view/Home.tsx
@@ -5,19 +5,15 @@ import {
    StyleSheet,
    ScrollView,
    TouchableOpacity,
+   Dimensions,
 } from "react-native";
 
-import Constants from "expo-constants";
 import MenuWithBtns from "../elements/MenuWithBtns";
 import MenuWithList from "../elements/MenuWithList";
-import { temp_data } from "../../elements/data";
 import Icon from "react-native-vector-icons/Ionicons";
 import { BottomTabNavigation } from "../../../screens/SearchScreen";
-import { RootState } from "../../../modules";
-import { useDispatch, useSelector } from "react-redux";
-import {
-   logout
-} from "../../../lib/api/auth";
+import { logout } from "../../../lib/api/auth";
+import constants from "../../../lib/utils/constants";
 
 const articleTopStyleConst = {
    parent: {
@@ -67,19 +63,24 @@ function Home({ navigation, auth }: Props) {
                   <View style={styles.header}>
                      <Text style={styles.headerText}>{auth?.centerName}</Text>
                      <View style={styles.headerIcon}>
-                        <Icon
-                           name={"notifications-outline"}
-                           size={35}
-                           color="white"
-                           style={styles.icon}
-                           onPress={() => console.log('hello')}
-                        />
+                        {auth.authority === constants.authority_director && (
+                           <Icon
+                              name={"notifications-outline"}
+                              size={35}
+                              color="white"
+                              style={styles.icon}
+                              onPress={() => navigation.navigate("Notice")}
+                           />
+                        )}
                         <Icon
                            name={"log-out-outline"}
                            size={35}
                            color="white"
                            style={styles.icon}
-                           onPress={() => [logout(),navigation.navigate("Auth")]}
+                           onPress={() => [
+                              logout(),
+                              navigation.navigate("Auth"),
+                           ]}
                         />
                      </View>
                   </View>
@@ -105,21 +106,44 @@ function Home({ navigation, auth }: Props) {
                </View>
             </ScrollView>
          ) : (
-            <View style={styles.blankContainer}>
-               <Text style={styles.blankText}>등록된 유치원이 없습니다. </Text>
-               <Text style={styles.blankText}>먼저 등록을 해야 서비스를 </Text>
-               <Text style={styles.blankText}>이용할 수 있습니다. </Text>
-               {auth.authority == "TEACHER" && (
-                  <TouchableOpacity
-                     style={styles.blackBtn}
-                     onPress={() => navigation.navigate("Kinger")}>
-                     <Text style={[styles.blankText, styles.blankBtnText]}>
-                        유치원 인증하기
-                     </Text>
-                  </TouchableOpacity>
-               )}
-                     
-               
+            <View style={{ flex: 1 }}>
+               <View
+                  style={[
+                     styles.header,
+                     {
+                        position: "absolute",
+                        top: 0,
+                        left: Dimensions.get("window").width,
+                     },
+                  ]}>
+                  <View style={styles.headerIcon}>
+                     <Icon
+                        name={"log-out-outline"}
+                        size={35}
+                        color="black"
+                        style={styles.icon}
+                        onPress={() => [logout(), navigation.navigate("Auth")]}
+                     />
+                  </View>
+               </View>
+               <View style={styles.blankContainer}>
+                  <Text style={styles.blankText}>
+                     등록된 유치원이 없습니다.{" "}
+                  </Text>
+                  <Text style={styles.blankText}>
+                     먼저 등록을 해야 서비스를{" "}
+                  </Text>
+                  <Text style={styles.blankText}>이용할 수 있습니다. </Text>
+                  {auth.authority == "TEACHER" && (
+                     <TouchableOpacity
+                        style={styles.blackBtn}
+                        onPress={() => navigation.navigate("Kinger")}>
+                        <Text style={[styles.blankText, styles.blankBtnText]}>
+                           유치원 인증하기
+                        </Text>
+                     </TouchableOpacity>
+                  )}
+               </View>
             </View>
          )}
       </View>
@@ -127,95 +151,91 @@ function Home({ navigation, auth }: Props) {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff',
-    width: '100%'
-  },
-  blankContainer:{
-    justifyContent: 'center',
-    alignItems:'center'
-  },
-  blankText:{
-    fontSize:25,
-    paddingBottom:10
-  },
-  blackBtn:{
-    marginTop:10,
-    borderRadius:10,
-    backgroundColor:'#ffd257',
-    width:150,
-    paddingTop:5,
-    height:50,
-    justifyContent: 'center',
-    
-  },
-  blankBtnText:{
-    fontSize:18,
-    textAlign:'center',
-    color:'#fff',
-    fontWeight:'bold'
-  },
-  header:{
-    height:50,
-    alignItems:'center',
-    flexDirection: 'row',
-    marginBottom: 24,
-  },
-  headerText:{
-    fontSize:28,
-    fontFamily:'Font',
-    color: 'white'
-  },
-  headerIcon:{
-    position:'absolute',
-    right:10,
-    flexDirection:'row'
-  },
-  headerIconText:{
-    color:'red',
-    
-  },
-  icon:{
-   fontSize:30,
-   paddingLeft:10,
-  },
-  articleTop:{
-    backgroundColor: '#FFD257',
-    paddingHorizontal: 16,
-    paddingTop: 12,
-  },
-  articleTopText:{
-    fontSize:20,
-    width:250,
-    fontFamily:'Font',
-    color: 'white',
-  },
-  menuWithBtns:{
-    width: '80%',
-    position: 'absolute',
-    top: 144,
-    marginHorizontal: '10%',
-    paddingTop: 8,
-    backgroundColor: '#FDFCF8',
-    borderRadius: 32,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-    elevation: 5,
-
-  },
-  menuWithList:{
-    paddingHorizontal: 24
-  },
-  
-
+   container: {
+      flex: 1,
+      justifyContent: "center",
+      // paddingTop: Constants.statusBarHeight,
+      backgroundColor: "#fff",
+      width: "100%",
+   },
+   blankContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   blankText: {
+      fontSize: 25,
+      paddingBottom: 10,
+   },
+   blackBtn: {
+      marginTop: 10,
+      borderRadius: 10,
+      backgroundColor: "#ffd257",
+      width: 150,
+      paddingTop: 5,
+      height: 50,
+      justifyContent: "center",
+   },
+   blankBtnText: {
+      fontSize: 18,
+      textAlign: "center",
+      color: "#fff",
+      fontWeight: "bold",
+   },
+   header: {
+      height: 50,
+      alignItems: "center",
+      flexDirection: "row",
+      marginBottom: 24,
+   },
+   headerText: {
+      fontSize: 28,
+      fontFamily: "Font",
+      color: "white",
+   },
+   headerIcon: {
+      position: "absolute",
+      right: 10,
+      flexDirection: "row",
+   },
+   headerIconText: {
+      color: "red",
+   },
+   icon: {
+      fontSize: 30,
+      paddingLeft: 10,
+   },
+   articleTop: {
+      backgroundColor: "#FFD257",
+      paddingHorizontal: 16,
+      paddingTop: 12,
+   },
+   articleTopText: {
+      fontSize: 20,
+      width: 250,
+      fontFamily: "Font",
+      color: "white",
+   },
+   menuWithBtns: {
+      width: "80%",
+      position: "absolute",
+      top: 144,
+      marginHorizontal: "10%",
+      paddingTop: 8,
+      backgroundColor: "#FDFCF8",
+      borderRadius: 32,
+      shadowColor: "#000",
+      shadowOffset: {
+         width: 0,
+         height: 5,
+      },
+      shadowOpacity: 0.34,
+      shadowRadius: 6.27,
+      elevation: 5,
+   },
+   menuWithList: {
+      paddingHorizontal: 24,
+   },
 });
 
 export default Home;
