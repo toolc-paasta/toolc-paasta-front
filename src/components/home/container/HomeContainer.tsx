6371769331b9dd5101fd37459ff43711aa030a0d
Seongil-Shin <callmeshin75@gmail.com>
2021-11-16 17:25:12 +0900

    notthing

--- src/components/home/container/HomeContainer.tsx
+++ src/components/home/container/HomeContainer.tsx
@@ -3,29 +3,25 @@ import { BottomTabNavigation } from "../../../screens/HomeScreen";
 import Home from "../view/Home";
 import { RootState } from "../../../modules";
 import { useDispatch, useSelector } from "react-redux";
-import { getNotice } from "../../../lib/api/forAdmin"
-import { getNotice_T } from "../../../lib/api/forAdmin"
-import { getNotice_D } from "../../../lib/api/forAdmin"
+import { getNotice } from "../../../lib/api/forAdmin";
+import { getNotice_T } from "../../../lib/api/forAdmin";
+import { getNotice_D } from "../../../lib/api/forAdmin";
 
 type Props = {
    navigation: BottomTabNavigation;
-}
+};
 function HomeContainer({ navigation }: Props) {
-
    const auth = useSelector(({ auth }: RootState) => auth);
    const [list, setList] = useState<any>();
 
-   const getData = async() => {
-      let data
-      if(auth.authority=='DIRECTOR')
-         data = await getNotice_D();
-      else if(auth.authority=='TEACHER')
-         data = await getNotice_T();
-      else
-         data = await getNotice();
-      setList(data)
-   }
-   useEffect(() => {      
+   const getData = async () => {
+      let data;
+      if (auth.authority == "DIRECTOR") data = await getNotice_D();
+      else if (auth.authority == "TEACHER") data = await getNotice_T();
+      else data = await getNotice();
+      setList(data);
+   };
+   useEffect(() => {
       getData();
    }, []);
 
