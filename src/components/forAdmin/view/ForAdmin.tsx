cb1767e35a7b2115311dfafc5687bd7d48f38921
Seongil-Shin <callmeshin75@gmail.com>
2021-11-17 14:44:28 +0900

    style : make fab button round

--- src/components/forAdmin/view/ForAdmin.tsx
+++ src/components/forAdmin/view/ForAdmin.tsx
@@ -1,151 +1,193 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Modal, Dimensions} from 'react-native';
-import Constants from 'expo-constants';
-import Header from '../../elements/Header'
+import React, { useState, useEffect } from "react";
+import {
+   Text,
+   View,
+   StyleSheet,
+   TouchableOpacity,
+   ScrollView,
+   Modal,
+   Dimensions,
+} from "react-native";
+import Constants from "expo-constants";
+import Header from "../../elements/Header";
 import { BottomTabNavigation } from "../../../screens/ForAdminScreen";
-import { classList,childList } from '../../elements/data';
+import { classList, childList } from "../../elements/data";
 import { FAB } from "react-native-elements";
-import ModalView from '../elements/Modal'
-import ModalView2 from '../elements/Modal2'
+import ModalView from "../elements/Modal";
+import ModalView2 from "../elements/Modal2";
 import { colors } from "../../elements/theme";
 
 type Props = {
-  navigation:BottomTabNavigation;
-  auth:any;
-  list:any;
-  nameList:any;
-  getListData:any;
+   navigation: BottomTabNavigation;
+   auth: any;
+   list: any;
+   nameList: any;
+   getListData: any;
 };
 
+export default function ForAdmin({
+   navigation,
+   auth,
+   list,
+   getListData,
+   nameList,
+}: Props) {
+   const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-export default function ForAdmin({navigation,auth,list,getListData,nameList}:Props) {
+   const DATA = list;
 
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
- 
-  const DATA = list
-
-  return (
-    <View style={[styles.container,modalVisible==true?{backgroundColor:'#bdbdbd'}:{backgroundColor:'#fff'}]}>
-        <Header header_title={auth.authority === 'DIRECTOR' ? '반 추가' : '어린이 추가'} navigation={navigation} setIsSubmit={null} IsInsert={null} setModalVisible={false}/>
-        <ScrollView style={styles.listContainer}>
-        {DATA == '' ? (
-          <Text style={{textAlign:'center',marginTop:200}}>등록된 데이터가 없습니다</Text>
-        ) : (
-          <>
-            {DATA?.map((item:any, i:number) => (
-              <View style={styles.list} key={i}>
-                <View>
-                  {auth.authority === 'DIRECTOR' ? (
-                    <Text style={styles.subText} numberOfLines={1}>{item.className} 반</Text>
-                  ) : (
-                    <Text style={styles.subText} numberOfLines={1}>{item.childName} 어린이</Text>                
-                  )}
-                  
-                </View>
-              </View>  
-            ))}
-          </>
-        )}
-        
-      </ScrollView>
-      <View style={styles.fabButtonWrap}>
-        <FAB
-          visible={true}
-          raised
-          icon={{
-              name: "plus",
-              type: "font-awesome",
-              color: "white"
-          }}
-          buttonStyle={styles.fabButton}
-          onPress={() => setModalVisible(true)}
-        />
+   return (
+      <View
+         style={[
+            styles.container,
+            modalVisible == true
+               ? { backgroundColor: "#bdbdbd" }
+               : { backgroundColor: "#fff" },
+         ]}>
+         <Header
+            header_title={
+               auth.authority === "DIRECTOR" ? "반 추가" : "어린이 추가"
+            }
+            navigation={navigation}
+            setIsSubmit={null}
+            IsInsert={null}
+            setModalVisible={false}
+         />
+         <ScrollView style={styles.listContainer}>
+            {DATA == "" ? (
+               <Text style={{ textAlign: "center", marginTop: 200 }}>
+                  등록된 데이터가 없습니다
+               </Text>
+            ) : (
+               <>
+                  {DATA?.map((item: any, i: number) => (
+                     <View style={styles.list} key={i}>
+                        <View>
+                           {auth.authority === "DIRECTOR" ? (
+                              <Text style={styles.subText} numberOfLines={1}>
+                                 {item.className} 반
+                              </Text>
+                           ) : (
+                              <Text style={styles.subText} numberOfLines={1}>
+                                 {item.childName} 어린이
+                              </Text>
+                           )}
+                        </View>
+                     </View>
+                  ))}
+               </>
+            )}
+         </ScrollView>
+         <View style={styles.fabButtonWrap}>
+            <FAB
+               visible={true}
+               raised
+               icon={{
+                  name: "plus",
+                  type: "font-awesome",
+                  color: "white",
+               }}
+               containerStyle={{ backgroundColor: colors.primary }}
+               buttonStyle={styles.fabButton}
+               onPress={() => setModalVisible(true)}
+            />
+         </View>
+         <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+               setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.modalView}>
+               {auth.authority == "TEACHER" ? (
+                  <ModalView
+                     setModalVisible={setModalVisible}
+                     getListData={getListData}
+                     nameList={nameList}
+                  />
+               ) : (
+                  <ModalView2
+                     setModalVisible={setModalVisible}
+                     getListData={getListData}
+                     nameList={nameList}
+                  />
+               )}
+            </View>
+         </Modal>
       </View>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-      }}>
-        <View style={styles.modalView}>
-          {auth.authority== 'TEACHER'  ? 
-            <ModalView setModalVisible={setModalVisible} getListData={getListData} nameList={nameList}/> : 
-            <ModalView2 setModalVisible={setModalVisible} getListData={getListData} nameList={nameList}/>
-          }         
-        </View>
-      </Modal>
-    </View>
-  );
-};
+   );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff',
-  },
-  listContainer:{
-    padding: 15,
-  },
-  list:{
-    alignItems: 'center',
-    height:60,
-    paddingLeft:10,
-    flexDirection: 'row',
-    borderBottomWidth:1,
-    borderBottomColor: colors.secondary
-  },
-  mainText:{
-    fontWeight:'bold',
-    fontSize:15,
-    paddingBottom:10,
-    textAlign:'left',
-  },
-  subText:{
-    width:250,
-    fontSize:15,
-  },
-  numText:{
-    position:'absolute',
-    right:5,
-    fontSize:12,
-    textAlign:'right',
-    paddingTop:1,
-    width:50,
-    height:20
-
-  },
-  fabButtonWrap:{
-    position:'absolute',
-    bottom:25,
-    right:25,
-  },
-  fabButton: {
-    width: 60,
-    height: 60,
-    backgroundColor: colors.primary,
-    
- },
- modalView: {
-  height: Dimensions.get('window').height/10 * 3,
-  width: Dimensions.get('window').width/10*8,
-  position:'relative',
-  left:Dimensions.get('window').width/2-Dimensions.get('window').width/10*4,
-  top:Dimensions.get('window').height/2-Dimensions.get('window').height/10*2,
-  paddingLeft:16,
-  paddingRight:16,
-  backgroundColor:'#ffffff',
-  shadowColor: "#000",
-  shadowOffset: {
-    width: 0,
-    height: 2,
-  },
-  borderRadius:16,
-  shadowOpacity: 0.25,
-  shadowRadius: 3.84,
-  elevation: 5,
-},
+   container: {
+      flex: 1,
+      justifyContent: "center",
+      // paddingTop: Constants.statusBarHeight,
+      backgroundColor: "#fff",
+   },
+   listContainer: {
+      padding: 15,
+   },
+   list: {
+      alignItems: "center",
+      height: 60,
+      paddingLeft: 10,
+      flexDirection: "row",
+      borderBottomWidth: 1,
+      borderBottomColor: colors.secondary,
+   },
+   mainText: {
+      fontWeight: "bold",
+      fontSize: 15,
+      paddingBottom: 10,
+      textAlign: "left",
+   },
+   subText: {
+      width: 250,
+      fontSize: 15,
+   },
+   numText: {
+      position: "absolute",
+      right: 5,
+      fontSize: 12,
+      textAlign: "right",
+      paddingTop: 1,
+      width: 50,
+      height: 20,
+   },
+   fabButtonWrap: {
+      position: "absolute",
+      bottom: 25,
+      right: 25,
+   },
+   fabButton: {
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      backgroundColor: colors.primary,
+   },
+   modalView: {
+      height: (Dimensions.get("window").height / 10) * 3,
+      width: (Dimensions.get("window").width / 10) * 8,
+      position: "relative",
+      left:
+         Dimensions.get("window").width / 2 -
+         (Dimensions.get("window").width / 10) * 4,
+      top:
+         Dimensions.get("window").height / 2 -
+         (Dimensions.get("window").height / 10) * 2,
+      paddingLeft: 16,
+      paddingRight: 16,
+      backgroundColor: "#ffffff",
+      shadowColor: "#000",
+      shadowOffset: {
+         width: 0,
+         height: 2,
+      },
+      borderRadius: 16,
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+   },
 });
-
--- src/components/list/view/List.tsx
+++ src/components/list/view/List.tsx
@@ -1,163 +1,207 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Modal, Dimensions} from 'react-native';
-import Constants from 'expo-constants';
-import { list_parent } from '../../elements/data';
-import { list_notice } from '../../elements/data';
-import Header from '../../elements/Header'
-import ListDetail from './ListDetail'
+import {
+   Text,
+   View,
+   StyleSheet,
+   TouchableOpacity,
+   ScrollView,
+   Modal,
+   Dimensions,
+} from "react-native";
+import Constants from "expo-constants";
+import { list_parent } from "../../elements/data";
+import { list_notice } from "../../elements/data";
+import Header from "../../elements/Header";
+import ListDetail from "./ListDetail";
 import { BottomTabNavigation } from "../../../screens/NoticeBoardScreen";
-import { colors } from '../../elements/theme'
+import { colors } from "../../elements/theme";
 import { FAB } from "react-native-elements";
 
 type Props = {
-  headerTitle:string;
-  navigation:BottomTabNavigation;
-  auth:any;
-  list:any;
+   headerTitle: string;
+   navigation: BottomTabNavigation;
+   auth: any;
+   list: any;
 };
 
-const processText = (limit: number, text: string) => (
-  text.length > limit ?
-    text.slice(0, limit) + '...' :
-    text
-)
+const processText = (limit: number, text: string) =>
+   text.length > limit ? text.slice(0, limit) + "..." : text;
 
-export default function List({navigation, headerTitle,auth,list}:Props) {
+export default function List({ navigation, headerTitle, auth, list }: Props) {
+   const [date, setDate] = useState<any>(new Date());
+   const [data, setData] = useState<any>();
+   const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-  const [date,setDate] = useState<any>(new Date());
-  const [data,setData] = useState<any>();
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
+   const DATA = headerTitle === "공지 모아보기" ? list_notice : list_parent;
 
-  const DATA  = headerTitle==='공지 모아보기' ? list_notice : list_parent
+   const makeTime = (t: Date) => {
+      const Time2string = String(t).split("-");
+      const Time2string2 = Time2string[2].split(":");
+      const newTime = new Date(
+         Number(Time2string[0]),
+         Number(Time2string[1]) - 1,
+         Number(Time2string2[0].substring(0, 2)),
+         Number(Time2string2[0].substring(3, 5)),
+         Number(Time2string2[1]),
+         Number(Time2string2[2])
+      );
+      const sec = Math.floor(
+         (date.getTime() - (newTime.getTime() + 1000 * 60 * 60 * 9)) / 1000
+      );
+      let temp = sec;
+      let count = 0;
+      const unit = ["초", "분", "시간", "일"];
+      while (temp > 24) {
+         if (count < 2 && temp > 60) temp = Math.floor(temp / 60);
+         else if (count >= 2) temp = Math.floor(temp / 24);
+         else return temp + unit[count] + " 전";
+         count++;
+      }
+      return temp + unit[count] + " 전";
+   };
 
-  const makeTime = (t: Date) => {
-    const Time2string = String(t).split('-')
-    const Time2string2 = Time2string[2].split(':')
-    const newTime = new Date(Number(Time2string[0]), Number(Time2string[1])-1, Number(Time2string2[0].substring(0,2)), Number(Time2string2[0].substring(3,5)), Number(Time2string2[1]), Number(Time2string2[2]))    
-    const sec = Math.floor((date.getTime()-(newTime.getTime()+1000*60*60*9))/1000)
-    let temp = sec
-    let count = 0
-    const unit = ['초','분','시간','일']
-    while(temp>24){
-      if(count<2 && temp>60)
-        temp = Math.floor(temp/60)
-      else if(count>=2)
-        temp = Math.floor(temp/24)
-      else
-        return(temp+unit[count]+' 전')
-      count++
-    }
-    return(temp+unit[count]+' 전')
-    
-  }
-
-  return (
-    <View style={styles.container}>
-      <Header header_title={headerTitle} navigation={navigation} IsInsert={auth.authority == 'PARENT' ? null : true} setIsSubmit={null} setModalVisible={false}/>
-      <ScrollView style={styles.listContainer}>
-        {list == '' ? (
-          <Text style={{textAlign:'center',marginTop:100}}>등록된 공지가 없습니다</Text>
-        ) : (
-          <>
-            {list && [...list].reverse().map((item:any, i:number) => (
-              <TouchableOpacity style={[styles.list, i % 2 ? styles.bgList : styles.list]} key={i} onPress={() => [setModalVisible(true),setData(item)]}>
-                <View>
-                  <Text style={styles.mainText}>[ {item.author == item.center.director.name ? '전체 공지' : '반 공지'} ]  {processText(13, item.title)}</Text>
-                  <Text style={styles.subText}>{processText(20, item.content)}</Text>
-                </View>
-                <Text style={styles.numText}>{makeTime(item.updatedAt)}</Text>
-              </TouchableOpacity>  
-            ))}
-          </>
-        )}
-      </ScrollView>
-      <View style={styles.fabButtonWrap}>
-        {auth.authority !== 'PARENT' && <FAB
-          visible={true}
-          raised
-          icon={{
-              name: "plus",
-              type: "font-awesome",
-              color: "white"
-          }}
-          buttonStyle={styles.fabButton}
-          onPress={() => navigation.navigate('UploadNotice')}
-        />}
+   return (
+      <View style={styles.container}>
+         <Header
+            header_title={headerTitle}
+            navigation={navigation}
+            IsInsert={auth.authority == "PARENT" ? null : true}
+            setIsSubmit={null}
+            setModalVisible={false}
+         />
+         <ScrollView style={styles.listContainer}>
+            {list == "" ? (
+               <Text style={{ textAlign: "center", marginTop: 100 }}>
+                  등록된 공지가 없습니다
+               </Text>
+            ) : (
+               <>
+                  {list &&
+                     [...list].reverse().map((item: any, i: number) => (
+                        <TouchableOpacity
+                           style={[
+                              styles.list,
+                              i % 2 ? styles.bgList : styles.list,
+                           ]}
+                           key={i}
+                           onPress={() => [
+                              setModalVisible(true),
+                              setData(item),
+                           ]}>
+                           <View>
+                              <Text style={styles.mainText}>
+                                 [{" "}
+                                 {item.author == item.center.director.name
+                                    ? "전체 공지"
+                                    : "반 공지"}{" "}
+                                 ] {processText(13, item.title)}
+                              </Text>
+                              <Text style={styles.subText}>
+                                 {processText(20, item.content)}
+                              </Text>
+                           </View>
+                           <Text style={styles.numText}>
+                              {makeTime(item.updatedAt)}
+                           </Text>
+                        </TouchableOpacity>
+                     ))}
+               </>
+            )}
+         </ScrollView>
+         <View style={styles.fabButtonWrap}>
+            {auth.authority !== "PARENT" && (
+               <FAB
+                  visible={true}
+                  raised
+                  icon={{
+                     name: "plus",
+                     type: "font-awesome",
+                     color: "white",
+                  }}
+                  buttonStyle={styles.fabButton}
+                  onPress={() => navigation.navigate("UploadNotice")}
+               />
+            )}
+         </View>
+         <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+               setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.modalView}>
+               <View>
+                  <ListDetail
+                     data={data}
+                     setModalVisible={setModalVisible}
+                     navigation={navigation}
+                     header_title={headerTitle}
+                  />
+               </View>
+            </View>
+         </Modal>
       </View>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-      }}>
-        <View style={styles.modalView}>
-          <View>
-            <ListDetail data={data} setModalVisible={setModalVisible} navigation={navigation} header_title={headerTitle}/>
-          </View>
-        </View>
-      </Modal>
-    </View>
-  );
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#fff',
-  },
-  listContainer:{
-    padding: 16
-  },
-  list:{
-    alignItems: 'center',
-    height:70,
-    flexDirection: 'row',
-    paddingHorizontal: 8
-  },
-  bgList: {
-    backgroundColor: colors.bgSecondary
-  },
-  mainText:{
-    fontSize:15,
-    paddingBottom:10,
-    textAlign:'left',
-  },
-  subText:{
-    width:250,
-    fontSize:13,
-    color: '#666666',
-  },
-  numText:{
-    position:'absolute',
-    right:8,
-    fontSize:12,
-    textAlign:'right',
-    paddingTop:1,
-    width:50,
-    height:20
-  },
-  articleMainText:{
-    fontSize:20,
-    paddingBottom:5,
-    fontFamily:'Font'
-  },
-  fabButtonWrap:{
-    position:'absolute',
-    bottom:25,
-    right:25,
-  },
-  fabButton: {
-    width: 60,
-    height: 60,
-    backgroundColor: colors.primary,
-    
-  }, 
-  modalView: {
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width,
-    backgroundColor:'#ffffff'
-  },
+   container: {
+      flex: 1,
+      justifyContent: "center",
+      // paddingTop: Constants.statusBarHeight,
+      backgroundColor: "#fff",
+   },
+   listContainer: {
+      padding: 16,
+   },
+   list: {
+      alignItems: "center",
+      height: 70,
+      flexDirection: "row",
+      paddingHorizontal: 8,
+   },
+   bgList: {
+      backgroundColor: colors.bgSecondary,
+   },
+   mainText: {
+      fontSize: 15,
+      paddingBottom: 10,
+      textAlign: "left",
+   },
+   subText: {
+      width: 250,
+      fontSize: 13,
+      color: "#666666",
+   },
+   numText: {
+      position: "absolute",
+      right: 8,
+      fontSize: 12,
+      textAlign: "right",
+      paddingTop: 1,
+      width: 50,
+      height: 20,
+   },
+   articleMainText: {
+      fontSize: 20,
+      paddingBottom: 5,
+      fontFamily: "Font",
+   },
+   fabButtonWrap: {
+      position: "absolute",
+      bottom: 25,
+      right: 25,
+   },
+   fabButton: {
+      width: 60,
+      height: 60,
+      backgroundColor: colors.primary,
+      borderRadius: 30,
+   },
+   modalView: {
+      height: Dimensions.get("window").height,
+      width: Dimensions.get("window").width,
+      backgroundColor: "#ffffff",
+   },
 });
